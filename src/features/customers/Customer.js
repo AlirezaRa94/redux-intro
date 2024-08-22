9a404bf07312b56323b1fe4a0860efc7b53f318b
Alireza Raei <alirezaraei17@gmail.com>
2024-08-22 21:34:27 +1000

    feat: Integrate Redux with Customer component
    - Added useSelector from react-redux in Customer.js
    - Updated Customer component to use fullName from Redux state
    - Wrapped App component with Provider in index.js to connect Redux store

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
+  const fullName = useSelector((state) => state.customer.fullName);
+  console.log(fullName);
   return <h2>👋 Welcome, %NAME%</h2>;
 }
 
